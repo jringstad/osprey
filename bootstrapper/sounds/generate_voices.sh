7b4a281be8c2806e11d70563253d5db9ab29983c
Jonathan Ringstad <jringstad@palantir.com>
2020-06-10 21:10:36 +0200

    circleci tests

--- bootstrapper/sounds/generate_voices.sh
+++ bootstrapper/sounds/generate_voices.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-function generate() {
+function generate_bootstrapper() {
   say -v Alex "$1" -o "$2"
   sox "$2".aiff "$2".wav
   rm "$2".aiff
 }
 
+function generate_diagnostics() {
+  say -v Samantha "$1" -o "$2"
+  sox "$2".aiff "$2".wav
+  rm "$2".aiff
+}
+
 rm -f *.wav
-generate "Bootstrapper initializing" initializing
-generate "checking uplink availability" uplink-test
-generate "failed to detect uplink" uplink-failed
-generate "using cellular uplink" uplink-cellular
-generate "using wired uplink" uplink-wired
-generate "using wifi uplink" uplink-wifi
-generate "installing system prerequisites" prerequisites
-generate "failed" generic-failure
-generate "succeeded" generic-success
-generate "downloading platform" downloading-platform
-generate "failed downloading platform release" downloading-failure
-generate "downloading diagnostics platform" downloading-platform
-generate "failed downloading diagnostics platform release" downloading-failure
-generate "acquiring initial configuration" configuration
-generate "failed to acquire initial configuration" configuration-failure
-generate "initializing platform diagnostics" diagnostics-initializing
-generate "failed to initialize platform diagnostics" diagnostics-failed
-generate "Diagnostics platform online" diagnostics-online
-generate "initializing platform" platform-initializing
-generate "failed to initialize platform" platform-failed
-generate "Platform online" platform-online
-generate "Bootstrapper finished successfully" exit
+generate_bootstrapper "Bootstrapper initializing" initializing
+generate_bootstrapper "checking uplink availability" uplink-test
+generate_bootstrapper "failed to detect uplink" uplink-failed
+generate_bootstrapper "using cellular uplink" uplink-cellular
+generate_bootstrapper "using wired uplink" uplink-wired
+generate_bootstrapper "using wifi uplink" uplink-wifi
+generate_bootstrapper "installing system prerequisites" prerequisites
+generate_bootstrapper "failed" generic-failure
+generate_bootstrapper "succeeded" generic-success
+generate_bootstrapper "downloading platform" downloading-platform
+generate_bootstrapper "failed downloading platform release" downloading-failure
+generate_bootstrapper "downloading diagnostics platform" downloading-platform
+generate_bootstrapper "failed downloading diagnostics platform release" downloading-failure
+generate_bootstrapper "acquiring initial configuration" configuration
+generate_bootstrapper "failed to acquire initial configuration" configuration-failure
+generate_bootstrapper "initializing platform diagnostics" diagnostics-initializing
+generate_bootstrapper "failed to initialize platform diagnostics" diagnostics-failed
+generate_bootstrapper "initializing platform" platform-initializing
+generate_bootstrapper "failed to initialize platform" platform-failed
+generate_bootstrapper "Osprey platform online" platform-online
+generate_bootstrapper "Bootstrapper finished successfully" exit
+
+
+generate_diagnostics "Diagnostics platform online" diagnostics-online
+generate_diagnostics "Engines armed" engines-armed
+generate_diagnostics "Engines disarmed" engines-disarmed
