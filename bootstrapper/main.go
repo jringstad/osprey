32abaee317480e04334058369cc76cdc1ba98fba
Jonathan Ringstad <jringstad@palantir.com>
2020-06-10 17:34:17 +0200

    starting on bootstrapper

--- bootstrapper/main.go
+++ bootstrapper/main.go
@@ -1,2 +1,43 @@
 package bootstrapper
 
+import (
+	"os/exec"
+	"strings"
+)
+
+/*
+- get environment variables (S3 bucket, EC2 gateway)
+- initialize
+- check internet connection
+- install prerequisites
+- download platform diagnostics
+- download platform
+- register with basestation
+- save ID
+- initialize platform diagnostics
+- initialize platform
+ */
+
+func runCommand(command string) string {
+	out, _ := exec.Command("bash", "-c", command).Output()
+	return strings.TrimSpace(string(out))
+}
+func log(message string, soundName string) {
+	exec.Command("aplay", "misc/" + soundName + ".wav").Run()
+}
+func checkConnectivity() {
+	// TODO: hit up ground station instead
+	err := exec.Command("ping", "-c", "4", "google.com").Run()
+	if err != nil {
+		log("failed to establish uplink", "uplink-failed")
+		panic("failed to establish uplink")
+	} else {
+		// TODO: detect actual uplink
+		log("Uplink established", "uplink-cellular")
+	}
+}
+
+func main() {
+	log("bootstrapper initializing", "initializing")
+	checkConnectivity()
+}
--- bootstrapper/misc/generate_voices.sh
+++ bootstrapper/misc/generate_voices.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
-
-VOICE=alex
+set -eu -o pipefail
 
 function generate() {
   say -v Alex "$1" -o "$2"
+  sox "$2".aiff "$2".wav
+  rm "$2".aiff
 }
 
-generate "Bootstrapper initializing" initializing.aiff
-generate "Bootstrapper failed" generic-failure.aiff
-generate "Bootstrapper succeeded" generic-success.aiff
-generate "Bootstrapper downloading release" downloading.aiff
-generate "Bootstrapper failed downloading release" downloading-failure.aiff
-generate "Bootstrapper acquiring initial configuration" configuration.aiff
-generate "Bootstrapper failed to acquire initial configuration" configuration-failure.aiff
+rm -f *.wav
+generate "Bootstrapper initializing" initializing
+generate "Bootstrapper checking uplink availability" uplink-test
+generate "Bootstrapper failed to detect uplink" uplink-failed
+generate "Bootstrapper using cellular uplink" uplink-cellular
+generate "Bootstrapper using wired uplink" uplink-wired
+generate "Bootstrapper using wifi uplink" uplink-wifi
+generate "Bootstrapper installing system prerequisites" prerequisites
+generate "Bootstrapper failed" generic-failure
+generate "Bootstrapper succeeded" generic-success
+generate "Bootstrapper downloading platform" downloading-platform
+generate "Bootstrapper failed downloading platform release" downloading-failure
+generate "Bootstrapper downloading diagnostics platform" downloading-platform
+generate "Bootstrapper failed downloading diagnostics platform release" downloading-failure
+generate "Bootstrapper acquiring initial configuration" configuration
+generate "Bootstrapper failed to acquire initial configuration" configuration-failure
+generate "Bootstrapper initializing platform diagnostics" diagnostics-initializing
+generate "Bootstrapper failed to initialize platform diagnostics" diagnostics-failed
+generate "Diagnostics platform online" diagnostics-online
+generate "Bootstrapper initializing platform" platform-initializing
+generate "Bootstrapper failed to initialize platform" platform-failed
+generate "Platform online" platform-online
+generate "Bootstrapper finished successfully" exit
