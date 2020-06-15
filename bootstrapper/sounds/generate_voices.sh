64d8131884e259eb30596c64d53725a7b8de8ccf
Jonathan Ringstad <jringstad@palantir.com>
2020-06-15 21:28:47 +0200

    removed comments

--- bootstrapper/sounds/generate_voices.sh
+++ bootstrapper/sounds/generate_voices.sh
@@ -14,16 +14,16 @@ function generate_diagnostics() {
 }
 
 rm -f *.wav
-generate_bootstrapper "Bootstrapper initializing" initializing # used
-generate_bootstrapper "Checking for key" key-checking # used
-generate_bootstrapper "Failed to find key" key-failure # used
-generate_bootstrapper "Key is present" key-success # used
-generate_bootstrapper "adding repository" adding-repo # used
-generate_bootstrapper "adding repository failed" adding-repo-failed # used
-generate_bootstrapper "self-updating bootstrapper" self-update # used
-generate_bootstrapper "installing platform" installing-platform # used
-generate_bootstrapper "failed to install packages" install-failure # used
-generate_bootstrapper "packages have been updated, rebooting" rebooting # used
-generate_bootstrapper "nothing needed updating" no-updates # used
-generate_bootstrapper "starting platform services" starting-platform-services # used
-generate_bootstrapper "bootstrapper finished" finished # used
+generate_bootstrapper "Bootstrapper initializing" initializing
+generate_bootstrapper "Checking for key" key-checking
+generate_bootstrapper "Failed to find key" key-failure
+generate_bootstrapper "Key is present" key-success
+generate_bootstrapper "adding repository" adding-repo
+generate_bootstrapper "adding repository failed" adding-repo-failed
+generate_bootstrapper "self-updating bootstrapper" self-update
+generate_bootstrapper "installing platform" installing-platform
+generate_bootstrapper "failed to install packages" install-failure
+generate_bootstrapper "packages have been updated, rebooting" rebooting
+generate_bootstrapper "nothing needed updating" no-updates
+generate_bootstrapper "starting platform services" starting-platform-services
+generate_bootstrapper "bootstrapper finished" finished
